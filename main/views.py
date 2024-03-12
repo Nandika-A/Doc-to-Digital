2d4f57533b142e68318aac64f1df0d57cbfad600
Nandika-A <nandikaagrawal610@gmail.com>
2024-03-12 22:51:01 +0530

    Web scrapping pseudo code

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+import main.scrapping_images as si
 # Create your views here.
+
+def index(request):
+    query_list = ["cat", "dog", "bird"]
+    si.search(query_list)
+    return render(request, 'index.html')

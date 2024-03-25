e96863ef36d40e63d61e2a02ca69d45ef24e3ac4
Nandika-A <nandikaagrawal610@gmail.com>
2024-03-26 01:52:18 +0530

    some changes

--- main/views.py
+++ main/views.py
@@ -16,13 +16,15 @@ def handle_uploaded_file(f):
     reader = PdfReader(f)
     path = os.path.join(BASE_DIR,'downloads')
     summary_pages = []
-
     if not os.path.exists(path):
         os.makedirs(path)
 
     for page in reader.pages:
         text = page.extract_text() 
-        summary = summary_per_page(text)
+        summary = summary_per_page(text) #pass this to text to speech
+        speech(summary)
+        #try to make it real time with websockets.
+        #display summary as "notes" of the doc on a page
         summary_pages.append(summary)
         count = 0
 
@@ -30,15 +32,22 @@ def handle_uploaded_file(f):
             with open(os.path.join(path, str(count) + image_file_object.name), "wb") as fp:
                 fp.write(image_file_object.data)
                 count += 1
-    
+
     return summary_pages
 
 def index(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            summary_pages = handle_uploaded_file(request.FILES["file"])
-            return render(request, "summary.html", {"summary_pages": summary_pages})
+            summary_page = handle_uploaded_file(request.FILES["file"])
+            return render(request, "summary.html", {"summary": summary_page})
     else:
         form = UploadFileForm()
     return render(request, "upload.html", {"form": form})
+
+def speech(summary):
+    """
+    This function gets the summary of each page and converts it to speech for each page.
+    This will be modified such that as soon as it completes the speech for one page, it will start the speech for the next page.
+    """
+    pass

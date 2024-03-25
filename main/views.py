6ca78758d4490e5e81bcdff0420ee6ec53b31324
Nandika-A <nandikaagrawal610@gmail.com>
2024-03-26 00:36:25 +0530

    show summary

--- main/views.py
+++ main/views.py
@@ -10,31 +10,35 @@ def summary_per_page(text):
     output = query({
         "inputs": text,
     })
-    print(output)
+    return output
 
 def handle_uploaded_file(f):
     reader = PdfReader(f)
     path = os.path.join(BASE_DIR,'downloads')
+    summary_pages = []
 
     if not os.path.exists(path):
         os.makedirs(path)
 
     for page in reader.pages:
         text = page.extract_text() 
-        summary_per_page(text)
+        summary = summary_per_page(text)
+        summary_pages.append(summary)
         count = 0
 
         for image_file_object in page.images:
             with open(os.path.join(path, str(count) + image_file_object.name), "wb") as fp:
                 fp.write(image_file_object.data)
                 count += 1
+    
+    return summary_pages
 
 def index(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES["file"])
-            return HttpResponseRedirect(".")
+            summary_pages = handle_uploaded_file(request.FILES["file"])
+            return render(request, "summary.html", {"summary_pages": summary_pages})
     else:
         form = UploadFileForm()
     return render(request, "upload.html", {"form": form})
